5fccd2fa676fffa95e65b7b263b61d9f2a3d6b7b
AntonioPando <antoniogpando@gmail.com>
2026-05-06 10:54:06 +0200

    feat: add dashboard component with routing and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { Movimientos } from './components/pages/movimientos/movimientos';
+import { Dashboard } from './components/pages/dashboard/dashboard';
 
 export const routes: Routes = [
-      {
-    path: 'movimientos',
-    component: Movimientos
-  }
+  { path: 'movimientos', component: Movimientos, title: 'Movimientos' },
+  { path: 'dashboard', component: Dashboard, title: 'Dashboard' },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/layout/header/header";
 import { MenuLateral } from "./components/layout/menu-lateral/menu-lateral";
+import { Dashboard } from './components/pages/dashboard/dashboard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, MenuLateral],
+  imports: [RouterOutlet, Header, MenuLateral, Dashboard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
