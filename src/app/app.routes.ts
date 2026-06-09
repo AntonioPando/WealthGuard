3cbbe9d2d070c26837136581f915ada8eded4911
Pilar Salvador <119239553+pilar098@users.noreply.github.com>
2026-06-09 20:42:31 +0200

    feat: add NotFound component for handling 404 errors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Dashboard } from './components/pages/dashboard/dashboard';
 import { Login } from './components/pages/login/login';
 import { Registro } from './components/pages/registro/registro';
 import { Presupuestos } from './components/pages/presupuestos/presupuestos';
+import { NotFound } from './components/pages/not-found/not-found';
 
 export const routes: Routes = [
   {
@@ -12,6 +13,8 @@ export const routes: Routes = [
     redirectTo: 'login',
     pathMatch: 'full'
   },
+  { path: '**', 
+    component: NotFound },
   {
     path: 'movimientos',
     component: Movimientos
