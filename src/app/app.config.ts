d998322c1354ea1f7465e32cbd35f40ee802d61e
pedrolopezmanera <pedrolopezmanera@gmail.com>
2026-06-10 18:27:38 +0200

    Revert "Merge pull request #14 from AntonioPando/feature/login"
    This reverts commit b07e8638415c8ab1a832384803ab5c7984e2d6d8, reversing
    changes made to f8793af1e2e903cbc17aab6de7c0c9b6e31d5a21.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,11 @@
-import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideHttpClient(),
-  
-
-    {provide: LOCALE_ID, useValue: 'es-Es'}
-
+    provideRouter(routes)
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/layout/header/header";
-import { MenuLateral } from "./components/layout/menu-lateral/menu-lateral";
-import { Dashboard } from './components/pages/dashboard/dashboard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, MenuLateral, Dashboard],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
